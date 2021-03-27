20d4cb8b888b51552e8c5daf32753d5bfc053876
hamza BENKHAY <hamza@geocene.com>
2021-03-27 09:33:02 +0100

    using only internal api

--- test-automation/run_newman.sh
+++ test-automation/run_newman.sh
@@ -4,28 +4,39 @@
 COLLECTION_ID=$(curl --location --request GET "https://api.getpostman.com/collections?apikey=${POSTMAN_API_KEY}" | \
 jq --arg COLLECTION_NAME "$COLLECTION_NAME" '.collections[] | select(.name==$COLLECTION_NAME) | .uid')
 
+ENVIRONMENT_ID=$(curl --location --request GET "https://api.getpostman.com/environments?apikey=${POSTMAN_API_KEY}" | \
+jq --arg ENVIRONMENT_NAME "$ENVIRONMENT_NAME" '.environments[] | select(.name==$ENVIRONMENT_NAME) | .uid')
+
 COLLECTION_ID=${COLLECTION_ID%\"}
 COLLECTION_ID="${COLLECTION_ID#\"}"
+ENVIRONMENT_ID=${ENVIRONMENT_ID%\"}
+ENVIRONMENT_ID="${ENVIRONMENT_ID#\"}"
 
 curl --location --request GET "https://api.getpostman.com/collections/$COLLECTION_ID?apikey=${POSTMAN_API_KEY}" \
 >> collection.json
 
+curl --location --request GET "https://api.getpostman.com/environments/$ENVIRONMENT_ID?apikey=${POSTMAN_API_KEY}" \
+>> postman_environment_variables.json
 
 #report newman results to Testrail by chunks (newman run tests on folders and update the existing test runs)
 
-while read line; do
+curl -H "Content-Type: application/json" -u "${TEST_RAIL_USERNAME}:${TEST_RAIL_PASSWORD}" \
+ "https://arabletestrail.testrail.io/index.php?/api/v2/get_runs/${TEST_RAIL_PROJECT_ID}" >> test_runs.json
 
-  FOLDER=$(echo $line | sed 's/[0-9]*,//')
-  export TESTRAIL_RUNID=$(echo $line | sed 's/,.*//')
+jq -c '.[]' test_runs.json | while read i; do
 
-  echo "start processing ${FOLDER}"
+  TEST_RUN_NAME=$(echo $i | jq '.name')
+  TEST_RUN_ID=$(echo $i | jq '.id')
+
+  echo "start processing ${TEST_RUN_NAME}"
 
   newman run collection.json \
-  --folder "$FOLDER" -e postman_environment_variables.json \
+  --folder "$TEST_RUN_NAME" -e postman_environment_variables.json \
   --reporters cli,testrail \
   --timeout-request 30000 \
   --timeout-script 30000
 
-  echo "processing ${FOLDER} finished"
+  echo "processing ${TEST_RUN_NAME} finished"
 
-done <postman_folders.txt
+
+done
